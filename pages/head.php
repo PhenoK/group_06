41f5287e43275e25830883c06afe1454ae502ce2
ShenJing <ShenJingnerve@gmail.com>
2016-05-22 23:20:11 +0800

    增加了metisMenu.css, js，將head.php引入的內容補上，並發現了jquery引入的資料夾路徑有誤，已修正

--- pages/head.php
+++ pages/head.php
@@ -7,6 +7,8 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<!-- MetisMenu CSS -->
+<link href="../css/metisMenu.css" rel="stylesheet">
 <!-- Timeline CSS -->
 <link href="../css/timeline.css" rel="stylesheet">
 <!-- Custom SB-Admin2 CSS -->
@@ -23,11 +25,12 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
-<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.0.min.js"></script>
+
 <!-- jQuery -->
-<script src="../js/jquery/dist/jquery.min.js"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.0.min.js"></script>
 <!-- Bootstrap Core JavaScript -->
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <!-- Custom Theme JavaScript -->
 <script src="../js/sb-admin-2.js"></script>
+<script src="../js/metisMenu.js"></script>
