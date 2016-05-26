0def78f7111941f57122d070ffdaed30287efc0f
katoreto <uzukiengel@gmail.com>
2016-05-26 19:08:10 +0800

    新增會員註冊表單送出後寫入資料庫之php

---
+++ pages/send.php
@@ -0,0 +1,26 @@
+<?php
+                
+        $link = mysqli_connect("localhost","root","123456","group_06")
+        or die("無法開啟MySQL資料庫連結!<br>");    
+
+        if (!$link ) {
+            echo "連結錯誤代碼: ".mysqli_connect_errno()."<br>";
+            echo "連結錯誤訊息: ".mysqli_connect_error()."<br>";
+            exit();
+        }
+        else
+        {
+            echo "success";
+            mysqli_query($link, 'SET CHARACTER SET utf8');
+            mysqli_query($link,"SET collation_connection = 'utf8_unicode_ci'");
+
+            $sql="INSERT INTO member (account,password,name,sex,birth,tel_no,addr,mail,nickname) VALUES ('" .$_POST['account']. "','" . 
+            $_POST['pwd'] . "','" . $_POST['user'] . "','" . $_POST['gender']  . "','" . $_POST['birth'] . "','" .
+            $_POST['tel'] . "','" . $_POST['addr'] . "','" . $_POST['mail']  . "','" . $_POST['nickname']."')";
+
+            mysqli_query($link, $sql);
+            mysqli_close($link);
+            header("location:index.php");
+        }
+
+?>
