aae63081725c5f3e8d90c06d54371d3d73a6e70a
katoreto <uzukiengel@gmail.com>
2016-06-03 01:23:53 +0800

    使用者修改留言之ajax導向頁面

---
+++ pages/msg_up.php
@@ -0,0 +1,30 @@
+<?php
+	include_once('initial.php');
+	include_once('connect.php');
+
+	$time=$_POST['time'];
+	$acc=$_SESSION['user'];
+	$newmsg=$_POST['edit_message'];
+
+	if(count(explode(" ", $newmsg)) - 1 >= strlen($newmsg))
+	{
+		$len=count(explode(" ", $newmsg)) - 1;
+		echo "<script language='JavaScript'>alert('請勿輸入空白內容');history.go(-1);</script>";
+	}
+	else
+	{	
+
+		mysqli_query($link,'SET CHARACTER SET utf8');
+		mysqli_query($link,"SET collation_connection = 'utf8_general_ci'");
+		
+		$newtime=date('Y-m-d G:i:s');
+		
+		$sql="UPDATE product_review SET text='$newmsg' WHERE time='$time' AND account='$acc'";
+		
+		
+		$result=mysqli_query($link,$sql);
+
+		echo " <script language='JavaScript'>history.go(-1);</script>";
+		
+	}
+?>
