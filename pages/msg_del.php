49c65e851ad9040728f8de8c48f3deb13c607b3b
katoreto <uzukiengel@gmail.com>
2016-06-03 01:22:07 +0800

    刪除商品留言之ajax導向頁面

--- pages/msg_del.php
+++ pages/msg_del.php
@@ -5,11 +5,11 @@
 	$time=$_POST['msg_time'];
 	$acc=$_SESSION['user'];
 
-	$sql="DELETE FROM product_review WHERE time='$time' ";
+	$sql="DELETE FROM product_review WHERE time='$time' AND account='$acc' ";
 	
 	mysqli_query($link,'SET CHARACTER SET utf8');
 	mysqli_query($link,"SET collation_connection = 'utf8_general_ci'");
 	$result=mysqli_query($link,$sql);
 
-	echo $result;
+
 ?>
