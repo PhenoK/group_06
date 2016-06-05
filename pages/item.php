f30d02b9d38ecfbe9d82251ef2f069b4224298f2
ShenJing <ShenJingnerve@gmail.com>
2016-06-05 20:53:16 +0800

    item.php被意外merge，已找回原來版本修正

--- pages/item.php
+++ pages/item.php
@@ -1,16 +1,13 @@
 <?php
 include_once 'initial.php';
-
 // 若url沒有傳入商品id，將導向至首頁
 if (!isset($_GET['id'])){
     header('Location: index.php');
 }
-
 if(@$_GET['page'])
     $page=$_GET['page'];
 else
     $page=1;
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +16,6 @@ else
     // 先從product table該id的資料中找出是哪一type(因為GET沒有傳type)
     $id = @$_GET['id'];
     $sql = "SELECT * FROM product WHERE id=$id";
-
     if ($result = mysqli_query($link, $sql)){
         // 若商品id不存在就導向回product.php
         if (mysqli_num_rows($result) == 0) {
@@ -35,53 +31,10 @@ else
     if ($result = mysqli_query($link, $sql)){
         $row = mysqli_fetch_assoc($result);
     }
-
     $result_rank=mysqli_query($link,"SELECT * FROM product_review WHERE product_id='$id'");
     $review_num=mysqli_num_rows($result_rank);
-
 ?>
 
-<<<<<<< HEAD
-        <head>
-            <!-- title:商品名稱 -->
-            <title>
-                <?=$row['name'] ?>
-            </title>
-            <?php include 'head.php'; ?>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-            <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script>
-            <script type="text/javascript"  src="additional-methods.min.js"></script>
-
-            <script>
-                $(document).ready(function($) {
-
-                    jQuery.validator.addMethod("none", function(value, element) {
-                        return value.split(" ").length - 1 < value.length;
-                    }, "請勿輸入空白內容");
-
-                    $("#form_msg").validate({
-                        submitHandler: function(form) {
-
-                            $(form).ajaxSubmit({
-                                type: "POST",
-                                url: "send_msg.php",
-                                dataType: "text",
-                                data: $('#form_msg').serialize(),
-
-                            });
-
-                        },
-
-                        errorPlacement: function(error, element) {
-
-                            if (element.is(':radio') || element.is(':checkbox')) {
-                                var eid = element.attr('name');
-                                $('input[name=' + eid + ']:last').next().after(error);
-                            } else {
-                                error.insertAfter(element);
-                            }
-                        },
-=======
 <head>
     <!-- title:商品名稱 -->
     <title>
@@ -113,7 +66,6 @@ else
                 alert(text);
                 return;
               }
-
             },
             error: function(xhr, ajaxOptions, thrownError) {
               alert("刪除失敗！");
@@ -133,27 +85,19 @@ else
               startColor: 'yellow',
               endColor: 'red'
             });
-
             jQuery.validator.addMethod("none", function(value, element) {
                 return value.split(" ").length - 1 < value.length;
             }, "請勿輸入空白內容");
-
             $("#form_msg").validate({
                 submitHandler: function(form) {
-
                     $(form).ajaxSubmit({
                         type: "POST",
                         url: "send_msg.php",
                         dataType: "text",
                         data: $('#form_msg').serialize(),
->>>>>>> ae390f9bd8fe19edb573362baf3c03577f3b5a33
-
                     });
-
                 },
-
                 errorPlacement: function(error, element) {
-
                     if (element.is(':radio') || element.is(':checkbox')) {
                         var eid = element.attr('name');
                         $('input[name=' + eid + ']:last').next().after(error);
@@ -161,70 +105,18 @@ else
                         error.insertAfter(element);
                     }
                 },
-
-
-
                 rules: {
-
                     message: {
-
                         none: true,
                         required: true,
-
                     }
-
                 },
-
                 messages: {
-
                     message: {
                         required: "請輸入留言",
                     }
-
-<<<<<<< HEAD
-                <style>
-                #section1 {
-                    height: 50px; // Set this height to the appropriate size
-                    overflow-y: scroll; // Only add scroll to vertical column
                 }
-                .error {
-                    color: #D82424;
-                    font-weight: normal;
-                    font-family: "微軟正黑體";
-                    display: block;
-                    padding: 1px;
-                }
-                textarea {
-                    resize: none;
-                }
-                
-                pre {
-                    margin-bottom: 5px;
-                }
-
-                
-                </style>
-        </head>
-
-        <body>
-            <div id="wrapper">
-                <?php include 'navbarTop.php'; ?>
-                <?php include 'navbarSide.php'; ?>
-                <div id="page-wrapper">
-                    <!-- Page Header -->
-                    <div class="row">
-                        <div class="col-sm-12 col-lg-12 col-md-12">
-                            <blockquote>
-                                <h3 class="page-header"><?=$row['name'] ?></h3>
-          </blockquote>
-        </div>
-      </div>
-      <!-- /.row -->
-=======
-                }
-
             });
-
         });
         </script>
 
@@ -233,11 +125,9 @@ else
             height: 50px; // Set this height to the appropriate size
             overflow-y: scroll; // Only add scroll to vertical column
         }
-
         textarea {
             resize: none;
         }
-
         pre {
             margin-bottom: 5px;
         }
@@ -258,7 +148,6 @@ else
               </div>
           </div>
           <!-- /.row -->
->>>>>>> ae390f9bd8fe19edb573362baf3c03577f3b5a33
 
       <!-- Product Info Row -->
       <div class="row">
@@ -301,7 +190,6 @@ else
                     </li>
                   <?php
                     break;
-
                   default:
                     break;
                 }
@@ -393,49 +281,6 @@ else
       <pre><?=mb_substr($row['content'], 0, strlen($row['content']))?></pre>
 
     <!-- Comment -->
-<<<<<<< HEAD
-    
-
-    <div class="well">
-       
-          <form method="POST" role="form" name="form_msg" id="form_msg" action="send_msg.php?id=<?=$id?>">
-            <div class="row">
-                <div class="form-group">
-                    <?php
-                        if(isset($_SESSION['user'])){
-                            echo "
-                                <div class='col-md-10'>
-                                    <textarea  name='message' id='message' rows='5' class='form-control'></textarea>            
-                                </div>";
-                        }
-                        else{
-                            echo "
-                                <div class='col-md-10 text-center'>
-                                    <pre style='margin:14px;'>請先登入以進行留言</pre>           
-                                </div>";
-                        }
-                    ?>
-                </div>    
-                
-                <div class="col-md-2 text-right">
-                    <?php 
-                        if(isset($_SESSION['user'])) 
-                            echo"<button type='submit' class='btn btn-success' >留言</button>";
-                        else
-                            echo "<a class='btn btn-success' href='signIn.php'>登入</a>";
-                    ?>
-                </div>
-            </div>
-          </form>
-              
-          <hr>
-
-          
-          <div class="embed-responsive embed-responsive-16by9" scrolling="auto">
-            <iframe src="msgarea.php?page=1&id=<?=$id?>">   
-            </iframe>        
-         </div>
-=======
 
 
       <div class="well">
@@ -471,7 +316,6 @@ else
 
         <hr>
 
->>>>>>> ae390f9bd8fe19edb573362baf3c03577f3b5a33
 
         <div class="embed-responsive embed-responsive-16by9">
           <iframe src="msgarea.php?page=1&id=<?=$id?>">
@@ -481,15 +325,10 @@ else
       </div>
       <!-- well -->
     </div>
-<<<<<<< HEAD
-    </div>
-    <!-- /#wrapper -->
-=======
     <!-- page-wrapper -->
   </div>
   <!-- /#wrapper -->
 
->>>>>>> ae390f9bd8fe19edb573362baf3c03577f3b5a33
     <?php include 'footer.php'; ?>
 </body>
 
