37d77c508bf059fddd145dbf566aefc870eb64b1
ShenJing <ShenJingnerve@gmail.com>
2016-06-06 01:11:14 +0800

    connect.php中增加淨化SQL, XSS注入的函式

--- pages/connect.php
+++ pages/connect.php
@@ -10,4 +10,12 @@ $link = mysqli_connect($sn, $un, $pw, $db) or die("資料庫連結有誤！<br>"
 mysqli_query($link, "SET CHARACTER SET utf8");
 mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");
 
+function mysql_entities_fix_string($link, $string){
+  return htmlentities(mysql_fix_string($link, $string));
+}
+function mysql_fix_string($link, $string){
+   if (get_magic_quotes_gpc())
+       $string = stripcslashes($string);
+   return mysqli_real_escape_string($link, $string);
+}
 ?>
